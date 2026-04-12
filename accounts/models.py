dce312330d51911984b1c4dfe1bdddcc5ff47c53
harrry <chegeharrison10@gmail.com>
2026-04-12 13:51:43 +0300

    Add base template and custom user model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+class CustomUser(AbstractUser):
+    USER_TYPE_CHOICES = (
+        ('admin', 'Admin'),
+        ('staff', 'Staff'),
+        ('customer', 'Customer'),
+    )
+
+    phone_number = models.CharField(max_length=20, blank=True, null=True)
+    user_type = models.CharField(max_length=20, choices=USER_TYPE_CHOICES, default='customer')
+
+    def __str__(self):
+        return self.username
